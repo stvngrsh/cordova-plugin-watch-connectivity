ed859ac53f0c167b995cad69e9ce62180244ed2e
Steven Gresh <stvngrsh@gmail.com>
2018-01-03 15:52:57 -0500

    Name changes

--- src/ios/CDVWatch.h
+++ src/ios/CDVWatch.h
@@ -1,7 +1,6 @@
 #import <Cordova/CDVPlugin.h>
 
 @interface CDVWatch : CDVPlugin {
-    NSString* test;
 }
 
 -(void)test:(CDVInvokedUrlCommand*)command;
--- www/watch.js
+++ www/watch.js
@@ -5,6 +5,7 @@ var Watch = function() {
 };
 
 Watch.test = function(param1, param2) {
+	console.log("In cordova plugin JS");
 	exec(successCallback, errorCallback, "Watch", "test", [param1, param2]);
 };
 
