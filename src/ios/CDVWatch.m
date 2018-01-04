8bf2c7be4eb780a413d4e943512b4b372b9ef93b
Steven Gresh <stvngrsh@gmail.com>
2018-01-03 23:42:29 -0500

    Added lots of error handling

--- src/ios/CDVWatch.m
+++ src/ios/CDVWatch.m
@@ -26,35 +26,75 @@
 
 -(void) sendMessage:(CDVInvokedUrlCommand*)command
 {
-	NSArray* args = [command arguments];
-	NSString* message = [args objectAtIndex:0];
+    __block CDVPluginResult* pluginResult = nil;
 
-    NSDictionary *payload = @{@"message":@"Test Message From Rider App"};
-    NSLog(@"%s", payload[@"message"]);
+    NSArray* args = [command arguments];
+    NSString* message = [args objectAtIndex:0];
+    NSLog(@"%@", message);
+    
+    NSDictionary *payload = @{@"message":message};
 
     [[WCSession defaultSession] sendMessage:payload
-                               replyHandler:nil
-                               errorHandler:nil
+                               replyHandler:^(NSDictionary *reply) {
+                                   pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+                               }
+                               errorHandler: ^(NSError *error) {
+                                   pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:error.localizedDescription];
+                               }
      ];
+    
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+
 }
 
 -(void) sendObject:(CDVInvokedUrlCommand*)command
 {
-	NSArray* args = [command arguments];
-	NSString* jsonString = [args objectAtIndex:0];
+    __block CDVPluginResult* pluginResult = nil;
 
-	NSDictionary *json = [NSJSONSerialization JSONObjectWithData:jsonString options:0 error:NULL];
-	NSLog(@"%s", json[@"test"]);
-
+    NSArray* args = [command arguments];
+    NSString* jsonString = [args objectAtIndex:0];
+    NSLog(@"%@", jsonString);
+    
+    NSData *jsonData = [jsonString dataUsingEncoding:NSUTF8StringEncoding];
+    NSDictionary *payload = [NSJSONSerialization JSONObjectWithData:jsonData options:0 error:NULL];
+    NSLog(@"%@", payload[@"test"]);
+    
+    [[WCSession defaultSession] sendMessage:payload
+                               replyHandler:^(NSDictionary *reply) {
+                                   pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+                               }
+                               errorHandler: ^(NSError *error) {
+                                   pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:error.localizedDescription];
+                               }
+     ];
+    
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    
 }
 
 -(void)updateAppContext:(CDVInvokedUrlCommand*)command
 {
-	NSArray* args = [command arguments];
-	NSString* jsonString = [args objectAtIndex:0];
+    CDVPluginResult* pluginResult = nil;
+    
+    NSArray* args = [command arguments];
+    NSString* jsonString = [args objectAtIndex:0];
+    NSLog(@"%@", jsonString);
+    
+    NSData *jsonData = [jsonString dataUsingEncoding:NSUTF8StringEncoding];
+    NSDictionary *payload = [NSJSONSerialization JSONObjectWithData:jsonData options:0 error:NULL];
+    NSLog(@"%@", payload[@"test"]);
 
-	NSDictionary *json = [NSJSONSerialization JSONObjectWithData:jsonString options:0 error:NULL];
-	[[WCSession defaultSession] updateApplicationContext:json error:nil]
+    NSError *error = nil;
+    BOOL success = [[WCSession defaultSession] updateApplicationContext:payload error:&error];
+    
+    if(success == NO) {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:error.localizedDescription];
+    }
+    else {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+    }
+    
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
 
 
@@ -64,7 +104,7 @@
 }
 
 -(void)checkAppContext:(CDVInvokedUrlCommand*)command{
-
+    
 }
 
 @end
